436611df3944841b0c32de04b0a53639b42985d7
TamTran72111 <tam.tran72111@gmail.com>
2020-05-08 21:19:25 -0400

    Implement merge sort

---
+++ benches/merge_sort.rs
@@ -0,0 +1,46 @@
+use clrs_rust::merge_sort::merge_sort;
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+fn bench_1000_shuffled_integers(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    let mut container: Vec<i32> = (0..1000).collect();
+    container.shuffle(&mut rng);
+    c.bench_function("merge sort 1000 shuffled elements", move |b| {
+        b.iter_batched(
+            || container.clone(),
+            |mut data| merge_sort(black_box(&mut data)),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+fn bench_1000_sorted_integers(c: &mut Criterion) {
+    let mut container: Vec<i32> = (0..1000).collect();
+    c.bench_function("merge sort 1000 sorted integers", |b| {
+        b.iter(|| merge_sort(black_box(&mut container)))
+    });
+}
+
+fn bench_1000_descending_sorted_integers(c: &mut Criterion) {
+    let container: Vec<i32> = (0..1000).rev().collect();
+    c.bench_function(
+        "merge sort 1000 descending sorted integers",
+        move |b| {
+            b.iter_batched(
+                || container.clone(),
+                |mut data| merge_sort(black_box(&mut data)),
+                BatchSize::SmallInput,
+            )
+        },
+    );
+}
+
+criterion_group!(
+    benches,
+    bench_1000_shuffled_integers,
+    bench_1000_sorted_integers,
+    bench_1000_descending_sorted_integers
+);
+criterion_main!(benches);
---
+++ src/merge_sort.rs
@@ -0,0 +1,101 @@
+pub fn merge_sort<T:PartialOrd + Copy>(container: &mut [T]) {
+  if container.len() > 1 {
+    let middle = (container.len() - 1) / 2;
+    merge_sort(&mut container[..=middle]);
+    merge_sort(&mut container[middle + 1..]);
+    _merge(container);
+  }
+}
+
+fn _merge<T:PartialOrd + Copy>(container: &mut [T]) {
+  let middle = (container.len() - 1) / 2;
+  let left: Vec<T> = (0..=middle).map(|i| container[i]).collect();
+  let right: Vec<T> = (middle + 1..container.len()).map(|i| container[i]).collect();
+
+  let mut k = 0;
+  let mut i = 0;
+  let mut j = 0;
+
+  while i < left.len() && j < right.len() {
+    if left[i] < right[j] {
+      container[k] = left[i];
+      i += 1;
+    } else {
+      container[k] = right[j];
+      j += 1;
+    }
+    k += 1;
+  }
+
+  while i < left.len() {
+    container[k] = left[i];
+    i += 1;
+    k += 1;
+  }
+
+  while j < right.len() {
+    container[k] = right[j];
+    j += 1;
+    k += 1;
+  }
+  
+}
+
+#[cfg(test)]
+mod test {
+    use super::merge_sort as sort;
+    use rand::seq::SliceRandom;
+    use rand::thread_rng;
+
+    #[test]
+    fn test_sort_an_empty_array() {
+        let mut container: [i32; 0] = [];
+        sort(&mut container);
+        assert_eq!(container, []);
+    }
+
+    #[test]
+    fn test_sort_one_element_array() {
+        let mut container = [1];
+        sort(&mut container);
+        assert_eq!(container, [1]);
+    }
+
+    #[test]
+    fn test_sort_an_unsorted_array() {
+        let mut container = [4, 1, 7, 2, 4, 3];
+        sort(&mut container);
+        assert_eq!(container, [1, 2, 3, 4, 4, 7]);
+    }
+
+    #[test]
+    fn test_sort_a_sorted_array() {
+        let mut container = [-4, 2, 3, 4, 4, 7];
+        sort(&mut container);
+        assert_eq!(container, [-4, 2, 3, 4, 4, 7]);
+    }
+
+    #[test]
+    fn test_sort_a_descending_array() {
+        let mut container = [4, 3, 0, -4, -6, -10];
+        sort(&mut container);
+        assert_eq!(container, [-10, -6, -4, 0, 3, 4]);
+    }
+
+    #[test]
+    fn test_sort_a_vec() {
+        let mut container = vec![3, 25, 23, 24, 4, 6, 1, 2];
+        sort(container.as_mut_slice());
+        assert_eq!(container, vec![1, 2, 3, 4, 6, 23, 24, 25]);
+    }
+
+    #[test]
+    fn test_sort_1000_shuffled_integers() {
+        let mut rng = thread_rng();
+        let mut container: Vec<i32> = (0..1_000).collect();
+        let sorted = container.clone();
+        container.shuffle(&mut rng);
+        sort(&mut container);
+        assert_eq!(container, sorted);
+    }
+}
