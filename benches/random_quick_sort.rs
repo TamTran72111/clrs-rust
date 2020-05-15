564ba93fa398410db35e260807a1a4b95654cecc
TamTran72111 <tam.tran72111@gmail.com>
2020-05-15 19:01:00 -0400

    Implement randomize quick sort

---
+++ benches/random_quick_sort.rs
@@ -0,0 +1,4 @@
+use clrs_rust::bench_sorting;
+use clrs_rust::quick_sort::random_quick_sort;
+
+bench_sorting!("random quick", random_quick_sort);
--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -1,9 +1,30 @@
+use rand::{thread_rng, Rng};
+
 pub fn quick_sort<T: PartialOrd>(arr: &mut [T]) {
     if arr.len() > 1 {
         let pivot = _partition(arr);
         quick_sort(&mut arr[..pivot]);
         quick_sort(&mut arr[pivot + 1..]);
     }
+}
+
+pub fn random_quick_sort<T: PartialOrd>(arr: &mut [T]) {
+    if arr.len() > 1 {
+        let pivot = _random_partition(arr);
+        random_quick_sort(&mut arr[..pivot]);
+        random_quick_sort(&mut arr[pivot + 1..]);
+    }
+}
+
+fn _random_partition<T: PartialOrd>(arr: &mut [T]) -> usize {
+    let mut rng = thread_rng();
+    let last_index = arr.len() - 1;
+    // random a point to be a pivot and swap it to the end
+    let pivot = rng.gen_range(0, last_index + 1);
+    if last_index != pivot {
+        arr.swap(last_index, pivot);
+    }
+    _partition(arr)
 }
 
 fn _partition<T: PartialOrd>(arr: &mut [T]) -> usize {
@@ -29,4 +50,12 @@ mod test {
     use crate::test_sorting;
 
     test_sorting!(quick_sort);
+}
+
+#[cfg(test)]
+mod test_random {
+    use super::random_quick_sort;
+    use crate::test_sorting;
+
+    test_sorting!(random_quick_sort);
 }
